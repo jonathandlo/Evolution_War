df88a574c1efa7199832a50aa7026851469e39b3
Jonathan Lo <jonathandlo@live.com>
2013-04-22 03:20:16 -0400

    initial commit added axiom d3d9 template project added axiom nupkg

---
+++ Evolution_War/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Evolution_War
+{
+	internal class Program
+	{
+		private static void Main()
+		{
+			try
+			{
+				System.Threading.Thread.CurrentThread.CurrentCulture = new System.Globalization.CultureInfo("en-US", false);
+				(new Game()).Run();
+			}
+			catch (Exception ex)
+			{
+				System.Diagnostics.Debug.WriteLine(Axiom.Core.LogManager.BuildExceptionString(ex));
+			}
+		}
+	}
+}
